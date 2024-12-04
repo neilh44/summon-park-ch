f5dd2d85cdf8b7065a21e860f49636acf91e1c51
neilh44 <nilesh.hanotia@outlook.com>
2024-12-04 16:11:00 +0530

    Delete bu_src directory

--- bu_src/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-// pages/index.tsx
-import React from "react";
-import JourneyChat from "./journey-chat"; // Ensure the path is relative to the 'pages' folder
-
-const Home = () => {
-  return <JourneyChat />;
-};
-
-export default Home;
